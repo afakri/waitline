424c8e0d24fe34191bb3f208a3029b36d45395fb
ayman fakri <61848289+afakri@users.noreply.github.com>
2024-01-12 14:34:45 +0100

    added LoginPage

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -15,6 +15,7 @@ import { GlobalStyle } from 'styles/global-styles';
 import { HomePage } from './pages/HomePage/Loadable';
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
 import { useTranslation } from 'react-i18next';
+import LoginPage from './pages/LoginPage';
 
 export function App() {
   const { i18n } = useTranslation();
@@ -30,6 +31,7 @@ export function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <GlobalStyle />
